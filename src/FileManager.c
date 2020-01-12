75a2ec6a204e6bffd58749db150b0f197aeb214f
Avery Aaron <halloweenavery@gmail.com>
2020-01-12 07:02:32 -0800

    Created .gitignore and added main.c and FileManager.c

---
+++ src/FileManager.c
@@ -0,0 +1,17 @@
+/*
+ *  FileManager.c
+ *  Nimble Game Engine
+ *
+ *  Created by Avery Aaron on 1/11/20.
+ *  Copyright © 2020 Avery Aaron. All rights reserved.
+ *
+ */
+
+#include "FileManager.h"
+
+
+// TODO
+
+
+// FileManger.c
+
