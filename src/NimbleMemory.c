142946ed2414050d1cc378fdabfc7ebcce91fbe8
Avery Aaron <halloweenavery@gmail.com>
2020-01-17 00:58:09 +0000

    NimbleMemory.c created

---
+++ src/NimbleMemory.c
@@ -0,0 +1,18 @@
+/*
+ *  NimbleMemory.c
+ *  Nimble Game Engine
+ *
+ *  Created by Avery Aaron on 1/11/20.
+ *  Copyright © 2020 Avery Aaron. All rights reserved.
+ *
+ */
+
+#include <stdlib.h>
+
+#include "NimbleMemory.h"
+
+
+uint64_t nimbleMemoryUsed = 0;
+
+
+// NimbleMemory.c
