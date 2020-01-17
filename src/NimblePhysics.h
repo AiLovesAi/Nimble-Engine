74c522e736bb1c2e80703d69409f82df09f80531
Avery Aaron <halloweenavery@gmail.com>
2020-01-17 01:14:34 +0000

    Renamed header

--- src/NimblePhysics.h
+++ src/NimblePhysics.h
@@ -1,5 +1,5 @@
 /*
- *  Physics.h
+ *  NimblePhysics.h
  *  Nimble Game Engine
  *
  *  Created by Avery Aaron on 1/11/20.
@@ -8,29 +8,29 @@
  */
 
 
-#ifndef Physics_h
-#define Physics_h
+#ifndef NimblePhysics_h
+#define NimblePhysics_h
 
 #include <cglm/cglm.h>
 #include <stdint.h>
 
-#include "WorldObjects.h"
+#include "NimbleWorldObjects.h"
 
 #define M_G 6.673889E-11
 
-struct physicsObject {
-    struct worldObject object;
+struct nimblePhysicsObject {
+    struct nimbleWorldObject object;
     double mass; // Kilograms
-    vec3 movementAxis;
-    double velocity; // Meters per second
-    double acceleration; // Meters per second per second
-    vec3 rotationalAxis;
-    double angularVelocity; // Radians per second
-    double angularAcceleration; // Randians per second per seconds
+    vec3 movementVector; // Stores movement axis with magnitude of velocity
+    float velocity; // Meters per second
+    float acceleration; // Meters per second per second
+    vec3 rotationalVector; // Stores rotational axis with magnitude of angular velocity
+    float angularVelocity; // Radians per second
+    float angularAcceleration; // Randians per second per seconds
 };
 
 // TODO
 
-#endif /* Physics_h */
+#endif /* NimblePhysics_h */
 
-// Physics.h
+// NimblePhysics.h
