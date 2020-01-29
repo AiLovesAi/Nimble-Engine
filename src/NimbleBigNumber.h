a2d7ee597b022f73324aaf0ce9b6f275340a5fc4
Avery Aaron <halloweenavery@gmail.com>
2020-01-29 03:39:14 +0000

    Switched typedef structs to use _t suffix

--- src/NimbleBigNumber.h
+++ src/NimbleBigNumber.h
@@ -24,77 +24,77 @@
 typedef struct BigInt {
     uint32_t * number;
     uint32_t size;
-} BigInt;
+} BigInt_t;
 
 typedef struct BigDec {
-    BigInt integer;
-    BigInt decimal;
+    BigInt_t integer;
+    BigInt_t decimal;
     uint32_t leadingZeros;
-} BigDec;
+} BigDec_t;
 
 /* BIG INTEGER */
 
 // NOTE: Big integers have the same format as a signed integer in little endian, with the sign bit as the MSB.
 // Returns a big integer defined as string.
-extern BigInt nimbleBigIntFromString(const char * string, uint32_t * error);
+extern BigInt_t nimbleBigIntFromString(const char * string, uint32_t * error);
 
 // Returns big integer x as a string.
-extern char * nimbleBigIntToString(const BigInt x, uint32_t * error);
+extern char * nimbleBigIntToString(const BigInt_t x, uint32_t * error);
 
 // Frees a big integer.
-ALWAYS_INLINE void nimbleBigIntFree(BigInt x)
+ALWAYS_INLINE void nimbleBigIntFree(BigInt_t x)
 {
     nimbleMemoryFree(x.number, x.size * sizeof(uint32_t));
     x.size = 0;
 }
 
 // Returns x + y.
-extern int32_t nimbleBigIntAdd(const BigInt x, const BigInt y, BigInt result);
+extern int32_t nimbleBigIntAdd(const BigInt_t x, const BigInt_t y, BigInt_t result);
 
 // Returns x - y.
-extern int32_t nimbleBigIntSub(const BigInt x, const BigInt y, BigInt result);
+extern int32_t nimbleBigIntSub(const BigInt_t x, const BigInt_t y, BigInt_t result);
 
 // Returns x * y.
-extern int32_t nimbleBigIntMul(const BigInt x, const BigInt y, BigInt result);
+extern int32_t nimbleBigIntMul(const BigInt_t x, const BigInt_t y, BigInt_t result);
 
 // Returns x / y.
-extern int32_t nimbleBigIntDiv(const BigInt x, const BigInt y, BigInt result);
+extern int32_t nimbleBigIntDiv(const BigInt_t x, const BigInt_t y, BigInt_t result);
 
 // Returns x % y.
-extern int32_t nimbleBigIntMod(const BigInt x, const BigInt y, BigInt result);
+extern int32_t nimbleBigIntMod(const BigInt_t x, const BigInt_t y, BigInt_t result);
 
 // Returns x ^ y.
-extern int32_t nimbleBigIntPow(const BigInt x, const BigInt y, BigInt result);
+extern int32_t nimbleBigIntPow(const BigInt_t x, const BigInt_t y, BigInt_t result);
 
 // Returns x == y.
-extern int32_t nimbleBigIntTET(const BigInt x, const BigInt y);
+extern int32_t nimbleBigIntTET(const BigInt_t x, const BigInt_t y);
 
 // Returns x > y.
-extern int32_t nimbleBigIntTGT(const BigInt x, const BigInt y);
+extern int32_t nimbleBigIntTGT(const BigInt_t x, const BigInt_t y);
 
 // Returns x < y.
-extern int32_t nimbleBigIntTLT(const BigInt x, const BigInt y);
+extern int32_t nimbleBigIntTLT(const BigInt_t x, const BigInt_t y);
 
 // Returns x >= y.
-extern int32_t nimbleBigIntTGE(const BigInt x, const BigInt y);
+extern int32_t nimbleBigIntTGE(const BigInt_t x, const BigInt_t y);
 
 // Returns x <= y.
-extern int32_t nimbleBigIntTLE(const BigInt x, const BigInt y);
+extern int32_t nimbleBigIntTLE(const BigInt_t x, const BigInt_t y);
 
 // Returns x as a BigDec.
-extern BigDec nimbleBigIntToBigDec(const BigInt x);
+extern BigDec_t nimbleBigIntToBigDec(const BigInt_t x);
 
 /* BIG DECIMAL */
 
 // NOTE: Big decimal consists of two big integers: one for the integer and one for the decimal. The decimal big integer does not have a signed bit.
 // Returns a big decimal defined as string.
-extern BigDec nimbleBigDecFromString(const char * string, uint32_t * error);
+extern BigDec_t nimbleBigDecFromString(const char * string, uint32_t * error);
 
 // Returns big decimal x as a string.
-extern char * nimbleBigDecToString(const BigDec x, uint32_t * error);
+extern char * nimbleBigDecToString(const BigDec_t x, uint32_t * error);
 
 // Frees a big decimal.
-ALWAYS_INLINE void nimbleBigDecFree(BigDec x)
+ALWAYS_INLINE void nimbleBigDecFree(BigDec_t x)
 {
     nimbleMemoryFree(x.integer.number, x.integer.size * sizeof(uint32_t));
     x.integer.size = 0;
@@ -103,43 +103,43 @@ ALWAYS_INLINE void nimbleBigDecFree(BigDec x)
 }
 
 // Returns x + y.
-extern int32_t nimbleBigDecAdd(const BigDec x, const BigDec y, BigDec result);
+extern int32_t nimbleBigDecAdd(const BigDec_t x, const BigDec_t y, BigDec_t result);
 
 // Returns x - y.
-extern int32_t nimbleBigDecSub(const BigDec x, const BigDec y, BigDec result);
+extern int32_t nimbleBigDecSub(const BigDec_t x, const BigDec_t y, BigDec_t result);
 
 // Returns x * y.
-extern int32_t nimbleBigDecMul(const BigDec x, const BigDec y, BigDec result);
+extern int32_t nimbleBigDecMul(const BigDec_t x, const BigDec_t y, BigDec_t result);
 
 // Returns x / y.
-extern int32_t nimbleBigDecDiv(const BigDec x, const BigDec y, BigDec result);
+extern int32_t nimbleBigDecDiv(const BigDec_t x, const BigDec_t y, BigDec_t result);
 
 // Returns x % y.
-extern int32_t nimbleBigDecMod(const BigDec x, const BigDec y, BigDec result);
+extern int32_t nimbleBigDecMod(const BigDec_t x, const BigDec_t y, BigDec_t result);
 
 // Returns x ^ y.
-extern int32_t nimbleBigDecPow(const BigDec x, const BigDec y, BigDec result);
+extern int32_t nimbleBigDecPow(const BigDec_t x, const BigDec_t y, BigDec_t result);
 
 // Returns x rounded up.
-extern int32_t nimbleBigDecCeil(const BigDec x, BigDec result);
+extern int32_t nimbleBigDecCeil(const BigDec_t x, BigDec_t result);
 
 // Returns x rounded down.
-extern int32_t nimbleBigDecFloor(const BigDec x, BigDec result);
+extern int32_t nimbleBigDecFloor(const BigDec_t x, BigDec_t result);
 
 // Returns x == y.
-extern int32_t nimbleBigDecTET(const BigDec x, const BigDec y);
+extern int32_t nimbleBigDecTET(const BigDec_t x, const BigDec_t y);
 
 // Returns x > y.
-extern int32_t nimbleBigDecTGT(const BigDec x, const BigDec y);
+extern int32_t nimbleBigDecTGT(const BigDec_t x, const BigDec_t y);
 
 // Returns x < y.
-extern int32_t nimbleBigDecTLT(const BigDec x, const BigDec y);
+extern int32_t nimbleBigDecTLT(const BigDec_t x, const BigDec_t y);
 
 // Returns x >= y.
-extern int32_t nimbleBigDecTGE(const BigDec x, const BigDec y);
+extern int32_t nimbleBigDecTGE(const BigDec_t x, const BigDec_t y);
 
 // Returns x <= y.
-extern int32_t nimbleBigDecTLE(const BigDec x, const BigDec y);
+extern int32_t nimbleBigDecTLE(const BigDec_t x, const BigDec_t y);
 
 #endif /* BigNumber_h */
 
