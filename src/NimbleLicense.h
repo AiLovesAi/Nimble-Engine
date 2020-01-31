b8978f94cbf7a0fa5e85000579e82db257edeb2f
Avery Aaron <halloweenavery@gmail.com>
2020-01-30 17:08:31 -0800

    Added NimbleLicense.h

---
+++ src/NimbleLicense.h
@@ -0,0 +1,35 @@
+/*
+ *  NimbleLicense.h
+ *  Nimble Game Engine
+ *
+ *  Created by Avery Aaron on 1/30/20.
+ *  Copyright (C) 2020 Avery Aaron <business.a3ology@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/* KEEP THIS INCLUDED IN ALL NIMBLE FILES OR INCLUDE THE RAW LICENSE IN YOUR BINARIES YOURSELF OR YOU WILL BE VIOLATING THE LICENSES */
+
+#ifndef NimbleLicese_h
+#define NimbleLicese_h
+
+// NOTE: Produced from xxd -i LICENSE.md
+
+extern const unsigned char LICENSE_md[];
+extern const unsigned int LICENSE_md_length;
+
+#endif /* NimbleLicese_h */
+
+// NimbleLicense.h
