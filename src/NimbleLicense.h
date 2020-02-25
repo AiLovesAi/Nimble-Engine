019478a1f940f10cb9c80d29aa3c5640fb2b136a
Avery Aaron <halloweenavery@gmail.com>
2020-02-25 15:23:52 -0800

    Removed GPL license again

--- src/NimbleLicense.h
+++ src/NimbleLicense.h
@@ -5,19 +5,6 @@
  *  Created by Avery Aaron on 1/30/20.
  *  Copyright (C) 2020 Avery Aaron <business.a3ology@gmail.com>
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
  */
 
 /* KEEP THIS INCLUDED IN ALL NIMBLE FILES OR INCLUDE THE RAW LICENSE IN YOUR BINARIES YOURSELF OR YOU WILL BE VIOLATING THE LICENSES */
