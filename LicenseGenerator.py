92fc8d9dfe83eac44bab93439ec5a23cf373a5e2
BuildTools <unconfigured@null.spigotmc.org>
2020-08-10 20:46:42 -0700

    Added versions to library; made a release builder python script

--- LicenseGenerator.py
+++ LicenseGenerator.py
@@ -1,10 +1,13 @@
 """
 
-  NimbleLicenseGenerator.py
+  LicenseGenerator.py
   Nimble Game Engine
 
   Created by Avery Aaron on 2020-08-08.
   Copyright (C) 2020 Avery Aaron <business.a3ology@gmail.com>
+
+  This program makes the NimbleLicense.c file from LICENSE.md that gets
+  included in every header file, and therefor all compiled executables in plain text.
 
 """
 
@@ -19,7 +22,7 @@ def main():
 *  NimbleLicense.c\n\
 *  Nimble Game Engine\n\
 *\n\
-*  Created automatically by NimbleLicenseGenerator.py on " + str(datetime.date(datetime.now())) + ".\n\
+*  Created automatically by LicenseGenerator.py on " + str(datetime.date(datetime.now())) + ".\n\
 *  Copyright (C) 2020 Avery Aaron <business.a3ology@gmail.com>\n\
 *\n\
 */\n\n\
@@ -53,4 +56,4 @@ const char LICENSE_md[] = \"\\n\\\n"
 if __name__ == "__main__":
     main()
 
-# NimbleLicenseGenerator.py
+# LicenseGenerator.py
---
+++ ReleaseBuilder.py
@@ -0,0 +1,25 @@
+"""
+
+  LicenseGenerator.py
+  Nimble Game Engine
+
+  Created by Avery Aaron on 2020-08-10.
+  Copyright (C) 2020 Avery Aaron <business.a3ology@gmail.com>
+
+  This program builds each program and library incrementally, then builds
+  the installers for Windows, MacOSX, and Linux, which are ready to be
+  distributed to github as a release.
+
+"""
+
+
+import os, sys
+from os import path
+
+def main():
+    # TODO
+
+if __name__ == "__main__":
+    main()
+
+# ReleaseBuilder.py
